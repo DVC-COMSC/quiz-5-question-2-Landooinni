c54dc4626b4189506ccf73c322b0e3bf1f769b7c
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-08 21:30:57 -0800

    (First Run)

--- main.cpp
+++ main.cpp
@@ -4,9 +4,26 @@ using namespace std;
 int main()
 {
    int N, M;
+   do
+   {
+      cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
+      cin >> N >> M;
+   } while (!(0 < N < 10 && 1 <= M <= 10));
    
    cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
    cin >> N >> M;
-   /* Complete your code */
-  
+   
+  for (int i = 0; i < M; i++)
+  {if (i>0)
+  {
+   for (size_t j = 0; j < i; j++)
+   {
+      cout<<N<<",";
+      N=N+N;
+   }
+  }else{
+   cout<<"1";
+  }
+  }
+  cout<< endl;
 }
