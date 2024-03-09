cccdd48cec88d895d91d0b6f2d16217b007a105f
Landooinni <97581425+Landooinni@users.noreply.github.com>
2024-03-08 21:38:56 -0800

    firsdt run

--- main.cpp
+++ main.cpp
@@ -8,21 +8,17 @@ int main()
    {
       cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
       cin >> N >> M;
-   } while (0 >= N >= 10 && 1 > M > 10);
-   
-   cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
-   cin >> N >> M;
-   
+   } while (0 >= N >= 10 || 1 > M > 10);
   for (int i = 0; i < M; i++)
   {if (i>0)
   {
    for (size_t j = 0; j < i; j++)
    {
       cout<<N<<",";
-      N=N+N;
+      N=N*N;
    }
   }else{
-   cout<<"1";
+   cout<<"1,";
   }
   }
   cout<< endl;
